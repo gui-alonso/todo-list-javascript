1602c4126349167ee2fe1668726a58a403235e80
Guilherme <alonsogui011@gmail.com>
2023-09-26 13:54:09 -0300

    add comments for project

--- scripts.js
+++ scripts.js
@@ -1,56 +1,86 @@
-const localStorageKey = 'to-do-list-ga'
+// Define a constant for the localStorage key used in the project
+const localStorageKey = 'to-do-list-ga';
 
-function validateIfExistNewTask(){
-    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
-    let inputValue = document.getElementById('input-new-task').value
-    let exist = values.find(x => x.name == inputValue)
-    return !exist ? false : true
+// Function to check if a new task with the same name already exists
+function validateIfExistNewTask() {
+    // Retrieve the task list from localStorage or create an empty array if it doesn't exist
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+
+    // Get the input value from the 'input-new-task' element
+    let inputValue = document.getElementById('input-new-task').value;
+
+    // Check if a task with the same name already exists in the list
+    let exist = values.find(x => x.name == inputValue);
+
+    // Return true if a task with the same name exists, otherwise return false
+    return exist ? true : false;
 }
 
+// Function to add a new task to the list
 function newTask() {
     let input = document.getElementById('input-new-task');
-    input.style.border = ''
+    input.style.border = '';
 
-    //validation
-    if(!input.value){
-        input.style.border = '2px solid red'
+    // Validation: Check if the input field is empty
+    if (!input.value) {
+        input.style.border = '2px solid red';
         alert('Digite algo para inserir na sua lista!');
     }
-    else if(validateIfExistNewTask()){
-        alert("Já existe uma task com esse nome.")
-    }
-    else{
-        // increment localStorage
-        let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    // Validation: Check if a task with the same name already exists
+    else if (validateIfExistNewTask()) {
+        alert("Já existe uma task com esse nome.");
+    } else {
+        // Add the new task to localStorage
+        let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
         values.push({
             name: input.value
-        })
-        localStorage.setItem(localStorageKey,JSON.stringify(values));
+        });
+        localStorage.setItem(localStorageKey, JSON.stringify(values));
+
+        // Display the updated task list
         showValues();
     }
-    //limpar campo após enviar task
-    input.value = ''
+
+    // Clear the input field after submitting the task
+    input.value = '';
 }
 
-function showValues(){
-    // JSON.parse é usado para analisar (converter de uma string JSON para um objeto JavaScript) os dados obtidos do localStorage.
-    // Se não houver dados no localStorage, um array vazio [] será usado como valor padrão.
-    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
-    let list = document.getElementById('to-do-list')
-    list.innerHTML = ''
-    for(let i = 0; i< values.length; i++){
+// Function to display the list of tasks
+function showValues() {
+    // Retrieve the task list from localStorage or create an empty array if it doesn't exist
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+
+    // Get the 'to-do-list' element
+    let list = document.getElementById('to-do-list');
+    
+    // Clear the existing list content
+    list.innerHTML = '';
+
+    // Iterate through the task list and display each task along with a button to remove it
+    for (let i = 0; i < values.length; i++) {
         list.innerHTML += `<li>${values[i]['name']}<button id='btn-ok' onclick='removeItem("${values[i]['name']}")'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
         <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
-      </svg></button></li>`
+        </svg></button></li>`;
     }
 }
 
-function removeItem(data){
-    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
-    let index = values.findIndex(x => x.name == data)
-    values.splice(index,1)
-    localStorage.setItem(localStorageKey,JSON.stringify(values));
-    showValues()
+// Function to remove a task from the list
+function removeItem(data) {
+    // Retrieve the task list from localStorage or create an empty array if it doesn't exist
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+
+    // Find the index of the task with the specified name
+    let index = values.findIndex(x => x.name == data);
+
+    // Remove the task from the list
+    values.splice(index, 1);
+
+    // Update the task list in localStorage
+    localStorage.setItem(localStorageKey, JSON.stringify(values));
+
+    // Display the updated task list
+    showValues();
 }
 
-showValues()
+// Initially display the task list when the page loads
+showValues();
